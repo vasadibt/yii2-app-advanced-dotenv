017ca866576619222c3b93823ce36d231516e994
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2021-01-14 10:59:59 +0100

    Codeception test fix

---
+++ common/config/codeception.php
@@ -0,0 +1,14 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/test.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => '',
+            ],
+        ],
+    ]
+);
