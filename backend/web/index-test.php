440a2f7623985275ad0d2aa67c11fb62f0d86b5e
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2021-01-14 08:50:33 +0100

    Change dotenv repository and fix integrations

--- backend/web/index-test.php
+++ backend/web/index-test.php
@@ -8,8 +8,6 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
 require __DIR__ . '/../../vendor/autoload.php';
-Dotenv\Dotenv::createMutable(__DIR__ . '/../../')->load();
-
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
--- backend/web/index.php
+++ backend/web/index.php
@@ -1,6 +1,5 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
-Dotenv\Dotenv::createMutable(__DIR__)->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG') || isset($_COOKIE[env('YII_DEBUG_COOKIE')]));
 defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
