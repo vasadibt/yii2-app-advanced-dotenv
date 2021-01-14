1e9863369790075626c1f907c68a5f8482282c31
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2021-01-14 11:15:32 +0100

    Fix tests

--- common/config/codeception.php
+++ common/config/codeception.php
@@ -7,7 +7,7 @@ return yii\helpers\ArrayHelper::merge(
         'components' => [
             'request' => [
                 // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-                'cookieValidationKey' => '',
+                'cookieValidationKey' => 'testkey',
             ],
         ],
     ]
--- frontend/tests/unit/models/ContactFormTest.php
+++ frontend/tests/unit/models/ContactFormTest.php
@@ -26,7 +26,7 @@ class ContactFormTest extends \Codeception\Test\Unit
         $emailMessage = $this->tester->grabLastSentEmail();
         expect('valid email is sent', $emailMessage)->isInstanceOf('yii\mail\MessageInterface');
         expect($emailMessage->getTo())->hasKey('admin@example.com');
-        expect($emailMessage->getFrom())->hasKey('noreply@example.com');
+        expect($emailMessage->getFrom())->hasKey('sender@example.com');
         expect($emailMessage->getReplyTo())->hasKey('tester@example.com');
         expect($emailMessage->getSubject())->equals('very important letter subject');
         expect($emailMessage->toString())->stringContainsString('body of current message');
